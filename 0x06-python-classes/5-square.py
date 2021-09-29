4f3b179dd99542be424040f3dbf1e4fa5dbe81bc
LauSCaicedo <3383@holbertonschool.com>
2021-09-29 17:37:57 -0500

    Printing a square

---
+++ 0x06-python-classes/5-square.py
@@ -0,0 +1,39 @@
+#!/usr/bin/python3
+"""calculate the square of a number"""
+
+
+from typing import Sized
+
+
+class Square:
+    """calculate the square of a number"""
+    @property
+    def size(self):
+        """getter func"""
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        """setter func"""
+        self.__size = value
+        if type(value) is not int:
+            raise TypeError("size must be an integer")
+        if value < 0:
+            raise TypeError("size must be >= 0")
+
+    def __init__(self, size=0):
+        """Square class"""
+        self.__size = size
+
+    def area(self):
+        """take the square of a nimber"""
+        return self.__size ** 2
+
+    def my_print(self):
+        if (self.__size) == 0:
+            print('\n', end="")
+        else:
+            for x in range(self.__size):
+                for z in range(self.__size):
+                    print("#", end="")
+                print('\n', end="")
