5c34a35505a10b853d8759b17b72516f215c1246
LauSCaicedo <3383@holbertonschool.com>
2021-09-29 00:02:08 -0500

    Access and update private attribute

---
+++ 0x06-python-classes/4-square.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""calculate the square of a number"""
+
+
+class Square:
+    """calculate the square of a number"""
+    @property
+    def size(self):
+        """getter func"""
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        """setter func"""
+        self.__size = value
+        if type(value) is not int:
+            raise TypeError("size must be an integer")
+        if value < 0:
+            raise TypeError("size must be >= 0")
+
+    def __init__(self, size=0):
