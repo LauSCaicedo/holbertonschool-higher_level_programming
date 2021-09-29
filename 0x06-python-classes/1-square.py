c41c7970a6e3147816811c5246607fef9eead069
LauSCaicedo <3383@holbertonschool.com>
2021-09-28 23:35:28 -0500

    Square with size

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""
+Define class called (Square).
+"""
+
+
+class Square:
+    """
+    Define class (Square).
+    """
+
+    def __init__(self, size):
+        """
+        Private instance attribute (Size).
+        """
+        self._size = size
