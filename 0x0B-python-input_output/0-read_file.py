1f385c3dcc9dc02bfeb18347b6263bf97fe593c1
LauSCaicedo <3383@holbertonschool.com>
2021-10-12 14:26:57 -0500

    Read file

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,6 +1,16 @@
 #!/usr/bin/python3
+'''
+function that reads a
+text file (UTF8) and
+prints it to stdout
+'''
+
+
 def read_file(filename=""):
-    with open(filename) as f:
+    '''
+    Use the with statement.
+    '''
+    with open(filename, encoding='UTF8') as f:
         Rd = f.read()
         print(Rd, end="")
     f.close()
