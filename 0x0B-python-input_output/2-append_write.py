88b290821a85416cdb6451fb1d6dfcd493ecd231
LauSCaicedo <3383@holbertonschool.com>
2021-10-12 17:19:52 -0500

    Append to a file

--- 0x0B-python-input_output/2-append_write.py
+++ 0x0B-python-input_output/2-append_write.py
@@ -1,5 +1,15 @@
 #!/usr/bin/python3
+'''
+Function that appends a string at the
+end of a text file (UTF8)
+and returns the number of characters added.
+'''
+
+
 def append_write(filename="", text=""):
+    '''
+    Use the with statement.
+    '''
     with open(filename, 'a', encoding='UTF8') as f:
         af = f.write(text)
         return af
