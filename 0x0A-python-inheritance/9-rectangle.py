402e8a80c64f984502b8f0eab3132c95bb5111d1
LauSCaicedo <3383@holbertonschool.com>
2021-10-13 12:29:38 -0500

    Full rectangle

--- 0x0A-python-inheritance/9-rectangle.py
+++ 0x0A-python-inheritance/9-rectangle.py
@@ -13,6 +13,9 @@ class Rectangle(BaseGeometry):
     '''
 
     def __init__(self, width, height):
+        '''
+        Constructor.
+        '''
         self.__width = width
         self.__height = height
         self.integer_validator('width', width)
