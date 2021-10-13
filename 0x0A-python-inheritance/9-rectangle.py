94a3b9721d789df1e3e790badc1255c592944190
LauSCaicedo <3383@holbertonschool.com>
2021-10-13 12:50:40 -0500

    Full rectangle

--- 0x0A-python-inheritance/9-rectangle.py
+++ 0x0A-python-inheritance/9-rectangle.py
@@ -1,18 +1,20 @@
 #!/usr/bin/python3
 """
-class Rectangle that inherits from BaseGeometry (7-base_geometry.py).
+Write a class Rectangle
+that inherits
+from BaseGeometry.
 """
 BaseGeometry = __import__('7-base_geometry').BaseGeometry
 
 
 class Rectangle(BaseGeometry):
     """
-        Type class of Rectangle inherit BaseGeometry
+        Class Rectangle that inherits.
     """
 
     def __init__(self, width, height):
         """
-        Constructor Magic method
+        Constructor.
         """
 
         self.__width = width
