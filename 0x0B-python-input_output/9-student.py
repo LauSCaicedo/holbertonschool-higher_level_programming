95f7f70b10b60fb44ef22530d3599f54fb8df3d1
LauSCaicedo <3383@holbertonschool.com>
2021-10-12 22:18:31 -0500

    Student to JSON

--- 0x0B-python-input_output/9-student.py
+++ 0x0B-python-input_output/9-student.py
@@ -1,5 +1,16 @@
 #!/usr/bin/python3
+'''
+Write a class
+Student that
+defines a student.
+'''
+
+
 class Student:
+    '''
+    Public instance attributes.
+    '''
+
     def __init__(self, first_name, last_name, age):
         self.first_name = first_name
         self.last_name = last_name
