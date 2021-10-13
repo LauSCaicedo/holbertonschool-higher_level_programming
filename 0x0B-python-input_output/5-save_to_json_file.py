8a9720216fb5e44fe53fcbc4df39629bfecb3d12
LauSCaicedo <3383@holbertonschool.com>
2021-10-12 21:54:48 -0500

    Class to JSON

--- 0x0B-python-input_output/5-save_to_json_file.py
+++ 0x0B-python-input_output/5-save_to_json_file.py
@@ -4,6 +4,7 @@ Function that writes an Object
 to a text file, using a
 JSON representation.
 '''
+import json
 
 
 def save_to_json_file(my_obj, filename):
--- 0x0B-python-input_output/8-class_to_json.py
+++ 0x0B-python-input_output/8-class_to_json.py
@@ -1,1 +1,14 @@
 #!/usr/bin/python3
+''' Function that returns the dictionary description
+with simple data structure
+(list, dictionary, string, integer and boolean)
+for JSON serialization of an object.
+'''
+
+
+def class_to_json(obj):
+    '''
+    Obj is an instance of a Class.
+    '''
+    your_dict = __dict__
+    return obj.your_dict
