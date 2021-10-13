0627994bdfd01768ae58a719158f72066f191d84
LauSCaicedo <3383@holbertonschool.com>
2021-10-12 23:13:26 -0500

    Load, add, save

--- 0x0B-python-input_output/10-student.py
+++ 0x0B-python-input_output/10-student.py
@@ -1,1 +1,17 @@
 #!/usr/bin/python3
+from typing import List
+
+
+class Student:
+    '''
+    Public instance attributes.
+    '''
+
+    def __init__(self, first_name, last_name, age):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        if type(attrs) is list(str):
+            return self.__dict__
