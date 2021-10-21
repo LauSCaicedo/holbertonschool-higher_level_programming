da2eba37ea27aca50b5880e6b3e66f1826aaaf62
LauSCaicedo <3383@holbertonschool.com>
2021-10-20 23:02:02 -0500

    Square update

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -39,3 +39,29 @@ class Square(Rectangle):
             raise ValueError("width must be > 0")
         self.width = value
         self.height = value
+
+    def update(self, *args, **kwargs):
+        '''
+            Method that assigns an argument to each attribute.
+        '''
+        if len(args) >= 1:
+            for x, elemen_t in enumerate(args):
+                if x == 0:
+                    self.id = elemen_t
+                if x == 1:
+                    self.size = elemen_t
+                if x == 2:
+                    self.x = elemen_t
+                if x == 3:
+                    self.y = elemen_t
+
+        else:
+            for key, value in kwargs.items():
+                if key == "size":
+                    self.size = value
+                elif key == "x":
+                    self.x = value
+                elif key == "y":
+                    self.y = value
+                elif key == "id":
+                    self.id = value
