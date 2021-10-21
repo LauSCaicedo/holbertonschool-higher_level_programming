b473e74a0301ea24f001d98f21770cec51ff9bf7
LauSCaicedo <3383@holbertonschool.com>
2021-10-20 21:13:14 -0500

    And now, the Square!

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -1,8 +1,20 @@
 #!/usr/bin/python3
+'''
+Creation of another
+class that inherits
+the class (Rectangle).
+'''
 from models.rectangle import Rectangle
 
 
 class Square(Rectangle):
+    '''
+    Init constructor.
+    '''
+
     def __init__(self, size, x=0, y=0, id=None):
+        '''
+        Call the super class.
+        '''
         super().__init__(size, size, x, y, id)
         self.size = size
