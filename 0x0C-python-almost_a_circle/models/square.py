177ea5062b137bd0b6e5228956eb31986a57b81d
LauSCaicedo <3383@holbertonschool.com>
2021-10-20 21:10:17 -0500

    And now, the Square!

---
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+from models.rectangle import Rectangle
+
+
+class Square(Rectangle):
+    def __init__(self, size, x=0, y=0, id=None):
+        super().__init__(size, size, x, y, id)
+        self.size = size
