c62bdab27e0199dfc00f94cd1c546fc92911d094
LauSCaicedo <3383@holbertonschool.com>
2021-11-17 22:59:32 -0500

    Always a name

--- 0x0E-SQL_more_queries/3-force_name.sql
+++ 0x0E-SQL_more_queries/3-force_name.sql
@@ -1,7 +1,2 @@
 -- Creates the table force_name on your MySQL server.
-CREATE TABLE IF NO EXISTS force_name(
-    id INT,
-    name VARCHAR(256) NOT NULL
-    ...
-    PRIMARY KEY (name)
-    );
+CREATE TABLE IF NO EXISTS force_name(id INT, name VARCHAR(256) PRIMARY KEY);
