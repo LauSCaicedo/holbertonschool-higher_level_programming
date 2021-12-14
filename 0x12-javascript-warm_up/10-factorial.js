70489ce271f91ab8a395f24b57fec80ab0752da4
LauSCaicedo <3383@holbertonschool.com>
2021-12-13 23:20:22 -0500

    Factorial

---
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -0,0 +1,13 @@
+#!/usr/bin/node
+const process = require('process');
+const myVar = parseInt(process.argv[2]);
+
+function myFact(n) {
+  if (isNaN(n) || (n === 1)) {
+    return (1);
+  } else {
+    return (n * myFact(n - 1));
+  }
+}
+result = myFact(myVar);
+console.log(result);
