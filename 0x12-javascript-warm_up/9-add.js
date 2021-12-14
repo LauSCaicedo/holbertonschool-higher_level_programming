3e081acac0c8797ab17fd60c2b947f7e0fbd2429
Laura Sofía Caicedo Henao <85470706+LauSCaicedo@users.noreply.github.com>
2021-12-13 22:59:52 -0500

    Update 9-add.js

--- 0x12-javascript-warm_up/9-add.js
+++ 0x12-javascript-warm_up/9-add.js
@@ -3,7 +3,7 @@ const process = require('process');
 const myVar = parseInt(process.argv[2]);
 const myVar2 = parseInt(process.argv[3]);
 
-function add(a, b) {
+function add (a, b) {
   return (a + b);
 }
 console.log(add(myVar, myVar2));
