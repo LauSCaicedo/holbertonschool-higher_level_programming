8116573b84020a56eac2f489e82db3b5d0741823
LauSCaicedo <3383@holbertonschool.com>
2021-12-13 22:40:23 -0500

    Square

--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -1,17 +1,12 @@
 #!/usr/bin/node
 const process = require('process');
 const size = process.argv[2];
-let z, y;
-let esp = '';
+let z;
 
 if (isNaN(process.argv[2])) {
   console.log('Missing number of occurrences');
 }
 
 for (z = 0; z < size; z++) {
-  for (y = 0; y < size; y++) {
-    esp += 'X';
-  }
-  esp += '\n';
+  console.log('X'.repeat(size));
 }
-console.log(esp);
