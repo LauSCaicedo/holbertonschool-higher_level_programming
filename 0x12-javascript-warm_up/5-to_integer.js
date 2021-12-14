89e873ca2295b315c49c968ba098317a7197f3fd
LauSCaicedo <3383@holbertonschool.com>
2021-12-13 20:17:58 -0500

    An Integer

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -1,7 +1,6 @@
 #!/usr/bin/node
-const { argv0 } = require('process');
 const process = require('process');
-convert = parseInt(process.argv[2]);
+const convert = parseInt(process.argv[2]);
 if (isNaN(process.argv[2])) {
   console.log('Not a number');
 } else {
