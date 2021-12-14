919f12c4a56f4d58e3ff20732751f847fbf583b9
LauSCaicedo <3383@holbertonschool.com>
2021-12-14 00:13:57 -0500

    Second biggest!

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -3,7 +3,7 @@ const process = require('process');
 const myVar = parseInt(process.argv.length);
 const order = process.argv.sort();
 let x = 0;
-if ((myVar <= 3)) {
+if (myVar <= 3) {
   console.log(0);
 } else {
   while (x !== myVar) {
