39f4cded1bab3457382e55a306cd379d2fde6ee5
Laura Sofía Caicedo Henao <85470706+LauSCaicedo@users.noreply.github.com>
2021-12-14 20:43:51 -0500

    Update 3-rectangle.js

--- 0x13-javascript_objects_scopes_closures/3-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/3-rectangle.js
@@ -1,13 +1,13 @@
 #!/usr/bin/node
 module.exports = class Rectangle {
-  constructor(w, h) {
+  constructor (w, h) {
     if (w > 0 && h > 0) {
       this.width = w;
       this.height = h;
     }
   }
 
-  print() {
+  print () {
     let z;
     for (z = 0; z < this.height; z++) {
       console.log('X'.repeat(this.width));
