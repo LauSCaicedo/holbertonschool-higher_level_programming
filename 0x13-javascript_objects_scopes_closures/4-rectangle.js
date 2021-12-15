f8e7cf8326302c9394e76c9be0ef19ff5906eccc
LauSCaicedo <3383@holbertonschool.com>
2021-12-14 21:26:34 -0500

    Rectangle #4

---
+++ 0x13-javascript_objects_scopes_closures/4-rectangle.js
@@ -0,0 +1,27 @@
+#!/usr/bin/node
+module.exports = class Rectangle {
+  constructor(w, h) {
+    if (w > 0 && h > 0) {
+      this.width = w;
+      this.height = h;
+    }
+  }
+
+  print() {
+    let z;
+    for (z = 0; z < this.height; z++) {
+      console.log('X'.repeat(this.width));
+    }
+  }
+
+  rotate() {
+    const j = this.height;
+    this.height = this.width;
+    this.width = j;
+  }
+
+  double() {
+    this.height *= 2;
+    this.width *= 2;
+  }
+};
