d513fa2d405502f487b37c1f5b63a8e1c9766bf8
LauSCaicedo <3383@holbertonschool.com>
2021-12-14 23:56:25 -0500

    Occurrences

---
+++ 0x13-javascript_objects_scopes_closures/7-occurrences.js
@@ -0,0 +1,12 @@
+#!/usr/bin/node
+
+exports.nbOccurences = function (list, searchElement) {
+  let x;
+  let cont = 0;
+  for (x = 0; x in list; x++) {
+    if (list[x] === searchElement) {
+      cont += 1;
+    }
+  }
+  return cont;
+}
