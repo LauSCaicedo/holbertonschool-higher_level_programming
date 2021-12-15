dde97ae627df8486a4c07ae11cbf54c5adbb40ff
Laura Sofía Caicedo Henao <85470706+LauSCaicedo@users.noreply.github.com>
2021-12-14 21:27:19 -0500

    Update 4-rectangle.js

--- 0x13-javascript_objects_scopes_closures/4-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/4-rectangle.js
@@ -1,26 +1,26 @@
 #!/usr/bin/node
 module.exports = class Rectangle {
-  constructor(w, h) {
+  constructor (w, h) {
     if (w > 0 && h > 0) {
       this.width = w;
       this.height = h;
     }
   }
 
-  print() {
+  print () {
     let z;
     for (z = 0; z < this.height; z++) {
       console.log('X'.repeat(this.width));
     }
   }
 
-  rotate() {
+  rotate () {
     const j = this.height;
     this.height = this.width;
     this.width = j;
   }
 
-  double() {
+  double () {
     this.height *= 2;
     this.width *= 2;
   }
