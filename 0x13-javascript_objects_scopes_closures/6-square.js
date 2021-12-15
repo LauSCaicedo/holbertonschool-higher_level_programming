3671ef302568ebc2dc131ee116c0b407d4ac6790
LauSCaicedo <3383@holbertonschool.com>
2021-12-14 22:54:07 -0500

    Square #1

---
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -0,0 +1,17 @@
+#!/usr/bin/node
+
+module.exports = class Square extends require('./5-square') {
+  charPrint(c) {
+    if (c === undefined) {
+      let j;
+      for (j = 0; j < this.height; j++) {
+        console.log('X'.repeat(this.width));
+      }
+    } else {
+      let z;
+      for (z = 0; z < this.height; z++) {
+        console.log('C'.repeat(this.width));
+      }
+    }
+  }
+};
