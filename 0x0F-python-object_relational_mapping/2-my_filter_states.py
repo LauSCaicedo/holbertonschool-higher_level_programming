c442848aeec1ffc5b8f999d9f07ccd64efe63b28
LauSCaicedo <3383@holbertonschool.com>
2021-12-19 23:36:45 -0500

    Update

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -18,7 +18,7 @@ if __name__ == "__main__":
     cursor.execute(
         "SELECT * \
         FROM states \
-        WHERE name LIKE '{:s}' \
+        WHERE name LIKE BYNARY '{:s}' \
         ORDER BY id ASC;".format(sys.argv[4]))
     rows = cursor.fetchall()
     for row in rows:
