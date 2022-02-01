b255305816b89920afa551500f2995c0bc31b00d
LauSCaicedo <3383@holbertonschool.com>
2022-02-01 18:56:18 -0500

    Readme

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -2,9 +2,9 @@
 const fs = require('fs');
 const argument = process.argv[2];
 
-fs.readFile(argument, 'utf-8', function(err, data) {
+fs.readFile(argument, 'utf-8', function (err, data) {
   if (err) {
-  console.log(err);
+    console.log(err);
   }
   console.log(data);
 });
